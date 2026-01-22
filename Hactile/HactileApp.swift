26b8c37c762276702a86ba8e0f7adc44c0accf52
kan <kanishk.s2015@gmail.com>
2026-01-22 18:36:13 +0530

    Initial Commit

---
+++ Hactile/HactileApp.swift
@@ -0,0 +1,17 @@
+//
+//  HactileApp.swift
+//  Hactile
+//
+//  Created by kan on 22/01/26.
+//
+
+import SwiftUI
+
+@main
+struct HactileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
